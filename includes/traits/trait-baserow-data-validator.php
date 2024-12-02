2328e76b7d4dbd1020a757ccd3270ab08b8f338f
Andrew <andrew@thetechshack.com.au>
2024-12-02 17:57:41 +0800

    We modified these files:
    includes/traits/trait-baserow-data-validator.php
    
    Changed $shipping_zones to $valid_shipping_zone_codes
    includes/shipping/class-baserow-shipping-zone-manager.php
    
    No actual changes made, but we were planning to modify it
    baserow-woo-importer.php
    
    Added file loading changes

--- includes/traits/trait-baserow-data-validator.php
+++ includes/traits/trait-baserow-data-validator.php
@@ -13,7 +13,7 @@ trait Baserow_Data_Validator_Trait {
     protected $validation_rules = [];
 
     /** @var array */
-    protected $valid_shipping_zone_codes = [
+    protected $shipping_zones = [
         'ACT', 'NSW_M', 'NSW_R', 'NT_M', 'NT_R',
         'QLD_M', 'QLD_R', 'REMOTE', 'SA_M', 'SA_R',
         'TAS_M', 'TAS_R', 'VIC_M', 'VIC_R', 'WA_M',
@@ -144,7 +144,7 @@ trait Baserow_Data_Validator_Trait {
         }
 
         // Validate shipping zones
-        foreach ($this->valid_shipping_zone_codes as $zone) {
+        foreach ($this->shipping_zones as $zone) {
             if (isset($data[$zone]) && !empty($data[$zone])) {
                 if (!is_numeric($data[$zone])) {
                     $this->log_error('Invalid shipping zone value', [
@@ -218,7 +218,7 @@ trait Baserow_Data_Validator_Trait {
             );
         }
 
-        $required_fields = array_merge(['is_bulky_item'], $this->valid_shipping_zone_codes);
+        $required_fields = array_merge(['is_bulky_item'], $this->shipping_zones);
 
         foreach ($required_fields as $field) {
             if (!isset($data[$field])) {

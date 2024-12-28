19f1bb3179ff2b7c2a1f774130b1bbc58665e3fc
Andrew <andrew@thetechshack.com.au>
2024-12-28 15:31:55 +0800

    fix: remove redundant _product_source meta field, using product_source taxonomy instead

--- includes/product/class-baserow-product-mapper.php
+++ includes/product/class-baserow-product-mapper.php
@@ -133,7 +133,6 @@ class Baserow_Product_Mapper {
             '_cost_price' => $cost_price,
             '_baserow_id' => $baserow_data['id'] ?? '',
             '_last_baserow_sync' => current_time('mysql'),
-            '_product_source' => 'DSZ'
         ];
 
         // Add EAN code if available
